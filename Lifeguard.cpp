b9a5358f36d6d30b87c1c95ab996dc501acd4528
Travis McCormick <mccormicktravis1110@gmail.com>
2023-08-21 13:57:26 -0400

    Added more comments to the Lifeguard class files because i forgot to include them last time

--- Lifeguard.cpp
+++ Lifeguard.cpp
@@ -1,63 +1,72 @@
 #include "Lifeguard.h"
 
+// Constructor for Lifeguard class
 Lifeguard::Lifeguard(string num, int hour, int min, int guard)
 {
-
-    name = num;
+    name = num; // Initialize the lifeguard's name
     cout << endl;
 
-    setShiftTime(hour, min);
+    setShiftTime(hour, min); // Set the lifeguard's shift time
 }
 
+// Destructor for Lifeguard class
 Lifeguard::~Lifeguard()
-{}
+{
+    // Destructor doesn't need additional actions for this class
+}
 
+// Setter for lifeguard's name
 void Lifeguard::setName(string num)
 {
-    this->name = num;
+    this->name = num; // Update the lifeguard's name
 }
 
+// Getter for lifeguard's name
 string Lifeguard::getName() const
 {
-    return name;
+    return name; // Return the lifeguard's name
 }
 
+// Set lifeguard's shift time
 void Lifeguard::setShiftTime(int hours, int minutes)
 {
-    timeHour = hours;
-    timeMin = minutes;
+    timeHour = hours; // Set the lifeguard's shift hour
+    timeMin = minutes; // Set the lifeguard's shift minute
 }
 
-//Added these so i could easily set rotation...might get rid of this
+// Getter for lifeguard's shift hour
 int Lifeguard::getHour() const
 {
-    return timeHour;
+    return timeHour; // Return the lifeguard's shift hour
 }
 
+// Getter for lifeguard's shift minute
 int Lifeguard::getMin() const
 {
-    return timeMin;
+    return timeMin; // Return the lifeguard's shift minute
 }
 
+// Add time to lifeguard's shift
 void Lifeguard::addTime(int minutes)
 {
-    int totalMinutes = timeHour * 60 + timeMin;
-    totalMinutes += minutes;
+    int totalMinutes = timeHour * 60 + timeMin; // Convert current time to total minutes
+    totalMinutes += minutes; // Add the specified minutes
 
-    timeHour = totalMinutes / 60;
-    timeMin = totalMinutes % 60;
+    timeHour = totalMinutes / 60; // Update the lifeguard's shift hour
+    timeMin = totalMinutes % 60;  // Update the lifeguard's shift minute
 }
 
+// Subtract time from lifeguard's shift
 void Lifeguard::subtractTime(int minutes)
 {
-    int totalMinutes = timeHour * 60 + timeMin;
-    totalMinutes -= minutes;
+    int totalMinutes = timeHour * 60 + timeMin; // Convert current time to total minutes
+    totalMinutes -= minutes; // Subtract the specified minutes
 
     if (totalMinutes < 0)
     {
-        totalMinutes += 24 * 60; // Add a day's worth of minutes
+        totalMinutes += 24 * 60; // Add a day's worth of minutes to ensure positive value
     }
 
-    timeHour = totalMinutes / 60;
-    timeMin = totalMinutes % 60;
+    timeHour = totalMinutes / 60; // Update the lifeguard's shift hour
+    timeMin = totalMinutes % 60;  // Update the lifeguard's shift minute
 }
--- Lifeguard.h
+++ Lifeguard.h
@@ -12,31 +12,37 @@ using namespace std;
 
 class Lifeguard {
 public:
-    // Contructors
+    // Constructor with initial values
     Lifeguard(string num, int hour, int min, int guard = 2);
 
-    // Deconstructor
+    // Destructor
     ~Lifeguard();
 
-    // Getter and Setters
+    // Setter for lifeguard's name
     void setName(string num);
 
+    // Getter for lifeguard's name
     string getName() const;
 
+    // Set lifeguard's shift time
     void setShiftTime(int hours, int minutes);
 
+    // Getter for lifeguard's shift hour
     int getHour() const;
 
+    // Getter for lifeguard's shift minute
     int getMin() const;
 
+    // Add time to lifeguard's shift
     void addTime(int minutes);
 
+    // Subtract time from lifeguard's shift
     void subtractTime(int minutes);
 
 private:
-    string name;
-    int timeHour{};
-    int timeMin{};
+    string name;    // Lifeguard's name
+    int timeHour; // Lifeguard's shift hour
+    int timeMin;  // Lifeguard's shift minute
 };
 
 
